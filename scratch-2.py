c5204375d0d224b038d95d1756c7dcca645a7a28
Peter <pech6593@colorado.edu>
2022-07-10 01:00:05 -0600

    update

--- scratch-2.py
+++ scratch-2.py
@@ -520,8 +520,13 @@ osa = OSA.Data("Data/01-18-2022/SPECTRUM_GRAT_PAIR.CSV", False)
 osa.y = abs(osa.y)
 
 ret = Retrieval()
+
 ret.load_data("Data/01-24-2022/spctgm_grat_pair_output_better_aligned_2.txt")
 ret.set_signal_freq(367, 400)
+
+# ret.load_data("Data/01-17-2022/realigned_spectrometer_input.txt")
+# ret.set_signal_freq(284, 620)
+
 ret.correct_for_phase_matching()
 ret.set_initial_guess(1560, 10, 2 ** 12)
 ret.load_spectrum_data(osa.x * 1e-3, osa.y)
